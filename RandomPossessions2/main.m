d5e1050ac4ae97b0a86c48c80d14119d46aae3b7
Rashmi Bajaj <pszd8206@unknown78ca39b36c3d.att.net>
2013-01-09 14:52:36 -0800

    Initial Commit

---
+++ RandomPossessions2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomPossessions2
+//
+//  Created by Rashmi Bajaj on 1/9/13.
+//  Copyright (c) 2013 France Telecom Group (Orange) San Francisco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
